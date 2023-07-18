6c3df272f86297732b77ceb9e799a7fecba750ad
Jordan Kay <jordan@fleuronic.com>
2023-07-18 17:06:42 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,14 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
     name: "Caesura",
-    platforms: [
-        .iOS(.v16)
-    ],
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6)
+	],
     products: [
         .library(
             name: "Caesura",

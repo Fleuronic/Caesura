ad3f33a4cf72752648332b7bd357341198921683
Jordan Kay <jordan@fleuronic.com>
2024-01-25 17:06:34 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,24 +2,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "Caesura",
+	name: "Caesura",
 	platforms: [
 		.iOS(.v13),
 		.macOS(.v10_15),
 		.tvOS(.v13),
 		.watchOS(.v6)
 	],
-    products: [
-        .library(
-            name: "Caesura",
-            targets: ["Caesura"]
+	products: [
+		.library(
+			name: "Caesura",
+			targets: ["Caesura"]
 		)
-    ],
-    dependencies: [.package(url: "https://github.com/Fleuronic/Catenary", branch: "main")],
-    targets: [
-        .target(
-            name: "Caesura",
+	],
+	dependencies: [.package(url: "https://github.com/Fleuronic/Catenary", branch: "main")],
+	targets: [
+		.target(
+			name: "Caesura",
 			dependencies: ["Catenary"]
 		)
-    ]
+	]
 )
